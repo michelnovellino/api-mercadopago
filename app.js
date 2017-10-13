a9b1ddbc0780f5b36737aec3bd893600854140b1
Luis Torres <luisdaniel.programador@gmail.com>
2017-10-13 12:09:50 -0400

    a

--- app.js
+++ app.js
@@ -18,14 +18,16 @@ app.use(bodyParser.json());
 var oldAccessToken = mercadopago.configurations.getAccessToken();
 
 mercadopago.configure({
-  sandbox: true,
-  access_token: config.access_token
+  client_id: config.client_id,
+  client_secret: config.client_secret
 });
 
 var payment = {
   description: 'Buying a PS4',
   transaction_amount: 10500,
-  payment_method_id: 'visa',
+
+  // Esto fue lo unico que cambie. 
+  payment_method_id: 'account_money',
   payer: {
     email: 'test_user_3931694@testuser.com',
     identification: {
@@ -39,9 +41,13 @@ app.get("/", function (req, res) {
   mercadopago.configurations.setAccessToken(config.access_token);
 
   mercadopago.payment.create(payment).then(function (data) {
+    console.log('true')
     res.send(JSON.stringify(data, null, 4));
   }).catch(function (error) {
+
+    console.log('false')
     res.send(error);
+
   }).finally(function () {
     mercadopago.configurations.setAccessToken(oldAccessToken);
   });
